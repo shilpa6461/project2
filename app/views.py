cc6da9fdfd685afda829d662cc57aa242e6f6064
mshilpa54321 <mshilpa54321@gmail.com>
2023-05-20 19:49:17 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def Pushpa(request):
+    return HttpResponse('Thaggedhele')
+
+
+def Amma(request):
+    return HttpResponse('I Love You Amma')
